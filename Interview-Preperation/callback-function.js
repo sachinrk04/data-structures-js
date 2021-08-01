8d6f2b7a7af9a2d6a899595dc316b0bd2f2591a4
Sachin Singh <sachin.innoventes@phonepe.com>
2021-08-01 11:38:16 +0530

    sort done

---
+++ Interview-Preperation/callback-function.js
@@ -0,0 +1,7 @@
+// What is a callback Function in JavaScript.
+
+// JavaScript is a synchronous and single-threaded language.
+
+// Blocking the main thread
+
+// Power of callback?
---
+++ Interview-Preperation/first-class-function.js
@@ -0,0 +1,23 @@
+
+// Function Statement = Function Declaration
+function a() {
+    console.log("a called")
+}
+
+// Function Expression
+var b = function() {
+    console.log("b called")
+}
+
+a()
+b()
+
+// Anonymous Function
+// function () {
+
+// }
+
+// Named Function Expression
+// Diff b/w Parameters & Arguments?
+// First Class Function
+// Arrow Function
